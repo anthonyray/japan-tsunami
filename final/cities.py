eceeb1f47cd6b54dec998d1e6c819a222c94fa9d
anthonyray <dantho361@hotmail.com>
2015-02-03 17:04:21 +0100

    Added Ids to cities

--- final/cities.py
+++ final/cities.py
@@ -1,12 +1,12 @@
 cities = [
-    ("Tokyo","9 128 090","http://en.wikipedia.org/wiki/Tokyo",35.732727,139.722404),
-    ("Yokohama","3 709 777","http://en.wikipedia.org/wiki/Yokohama",35.462635,139.774854),
-    ("Osaka","2 685 218","http://en.wikipedia.org/wiki/Osaka",34.705359,135.500729),
-    ("Nagoya","2 275 428","http://en.wikipedia.org/wiki/Nagoya",35.193866,136.907394),
-    ("Sapporo","1 919 684","http://en.wikipedia.org/wiki/Sapporo",43.179025,141.388028),
-    ("Kobe","1 538 281","http://en.wikipedia.org/wiki/Kobe",34.699714,135.187619),
-    ("Fukuoka","1 516 575","http://en.wikipedia.org/wiki/Fukuoka",33.643127,130.355035),
-    ("Kyoto","1 469 912","http://en.wikipedia.org/wiki/Kyoto",35.043493,135.771593),
-    ("Kawasaki","1 459 191","http://en.wikipedia.org/wiki/Kawasaki",35.557485,139.698357),
-    ("Saitama","1 250 407","http://en.wikipedia.org/wiki/Saitama",35.867481,139.642576)
+    (1,"Tokyo","9 128 090","http://en.wikipedia.org/wiki/Tokyo",35.732727,139.722404),
+    (2,"Yokohama","3 709 777","http://en.wikipedia.org/wiki/Yokohama",35.462635,139.774854),
+    (3,"Osaka","2 685 218","http://en.wikipedia.org/wiki/Osaka",34.705359,135.500729),
+    (4,"Nagoya","2 275 428","http://en.wikipedia.org/wiki/Nagoya",35.193866,136.907394),
+    (5,"Sapporo","1 919 684","http://en.wikipedia.org/wiki/Sapporo",43.179025,141.388028),
+    (6,"Kobe","1 538 281","http://en.wikipedia.org/wiki/Kobe",34.699714,135.187619),
+    (7,"Fukuoka","1 516 575","http://en.wikipedia.org/wiki/Fukuoka",33.643127,130.355035),
+    (8,"Kyoto","1 469 912","http://en.wikipedia.org/wiki/Kyoto",35.043493,135.771593),
+    (9,"Kawasaki","1 459 191","http://en.wikipedia.org/wiki/Kawasaki",35.557485,139.698357),
+    (10,"Saitama","1 250 407","http://en.wikipedia.org/wiki/Saitama",35.867481,139.642576)
 ]
