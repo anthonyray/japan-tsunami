2b1bb9472a15a5a88ce50ea69759f4e464fb7c87
root <root@docker-client.(none)>
2015-02-07 10:23:40 -0800

    sushi party

--- Cassandra/sushiProcess.py
+++ Cassandra/sushiProcess.py
@@ -9,14 +9,16 @@ alertPhones(dat_inf,dat_sup,lat,lon,radius,request_size=100000,table="bigtable4"
 '''
 
 # list : ((lat2,lon2,date,phone))
-date_inf_t = datetime(2015,1,5,10,32)
-date_sup_t = datetime(2015,1,10,10,30)
-lat_t = 36.055
-lon_t = 140.061
-radius_t = 500
+dateinfrecup = sys.argv[7].split('-')
+date_inf_t = datetime(int(dateinfrecup[0]),int(dateinfrecup[1]),int(dateinfrecup[2]),int(dateinfrecup[3]),int(dateinfrecup[4]))
+datesuprecup = sys.argv[8].split('-')
+date_sup_t = datetime(int(datesuprecup[0]),int(datesuprecup[1]),int(datesuprecup[2]),int(datesuprecup[3]),int(datesuprecup[4]))
+lat_t = float(sys.argv[5])
+lon_t = float(sys.argv[6])
+radius_t = int(sys.argv[9])
 size_req_t = 1000000
-table_t = 'bigtable'
-keyspace_t = 'japjapo'
+table_t = sys.argv[2]
+keyspace_t = sys.argv[1]
 
 hop = ca.alertPhones(date_inf_t,date_sup_t,lat_t,lon_t,radius_t,size_req_t,table_t,keyspace_t)
 print hop[0]
@@ -28,8 +30,8 @@ print hop[0:2]
 print "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
 
 
-crea_offset = sys.argv[1]
-crea_loop = sys.argv[2]
+crea_offset = sys.argv[3]
+crea_loop = sys.argv[4]
 offset = int(crea_offset)
 for i in range(0,int(crea_loop)):
     if(i == 0):
