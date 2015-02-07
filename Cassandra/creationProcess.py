94d85186558bde0f01a516be84aca71797fc0d65
anthonyray <dantho361@hotmail.com>
2015-02-07 20:46:02 +0100

    Cleaning scripts

--- Cassandra/creationProcess.py
+++ Cassandra/creationProcess.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
 """
-reated on Tue Jan 20 16:17:43 2015
-@author: Paco
-"""
-"""
 ------------------------------------------------
 --- Japan Tsunami Project
 --- Process 1
@@ -15,7 +11,6 @@ import sys
 import cassandraTest as ca
 from cassandra.cluster import Cluster
 
-# sys.argv[1]
 
 crea_keyspace = sys.argv[1]
 crea_table = sys.argv[2]
@@ -29,21 +24,3 @@ ca.createKeyspaceCassandra(session,keyspace=crea_keyspace)
 session = cluster.connect(crea_keyspace)
 ca.createTableCassandra(session,table=crea_table)
 ca.createTableCassandra(session,table=str(crea_table+'bis'))
-
-'''
-datacsv = ca.fromCSVtoDataframe(crea_file)
-
-offset = int(crea_offset)
-for i in range(0,int(crea_loop)):
-    if(i == 0):
-        test = ca.multipleInsertCreation(data=datacsv[0:offset],table=crea_table)
-        ca.multipleInsertExec(keyspace=crea_keyspace,cmd=test)
-        print "-------------------------------------------------"
-    else:
-        u = i*offset
-        uu = u + offset
-        test = ca.multipleInsertCreation(data=datacsv[u:uu],table=crea_table)
-        ca.multipleInsertExec(keyspace=crea_keyspace,cmd=test)
-        print "-------------------------------------------------"
-'''
-
