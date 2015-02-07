9eff3b694a196f2015133ff93b62d2c74588987e
anthonyray <dantho361@hotmail.com>
2015-02-07 20:01:18 +0100

    Cleaning scripts

--- Cassandra/cassandraTest.py
+++ Cassandra/cassandraTest.py
@@ -23,13 +23,13 @@ def createKeyspaceCassandra(session,keyspace=""):
 '''
 Create a special table into Cassandra database
 '''
-def createTableCassandra(session,table=""):    
-    session.execute("CREATE TABLE "+table+"(timestamp timestamp, codegsm text, latitude float, longitude float, phone text, PRIMARY KEY((timestamp,latitude,longitude)));") 
+def createTableCassandra(session,table=""):
+    session.execute("CREATE TABLE "+table+"(timestamp timestamp, codegsm text, latitude float, longitude float, phone text, PRIMARY KEY((timestamp,latitude,longitude)));")
     print "Table "+table+" created !";
-  
+
 '''
-Insert one row into special table
-'''  
+Insert one row into a table
+'''
 def insertCassandra(session,timestamp,latitude,longitude,table="",codegsm="",phone=""):
     session.execute("insert into "+table+"(timestamp,latitude,longitude,codegsm,phone) VALUES('"+str(timestamp)+"',"+str(latitude)+","+str(longitude)+",'"+str(codegsm)+"','"+str(phone)+"');")
 
@@ -44,19 +44,18 @@ def fillTable(session,data,table=""):
         longitude = data.iloc[i][3]
         phone = data.iloc[i][4]
         insertCassandra(session,timestamp,latitude,longitude,table,codegsm,phone)
-        print i
 
 '''
 Calculate Haversine distance of 2 points
 '''
 def haversine(lon1, lat1, lon2, lat2):
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
+    c = 2 * asin(sqrt(a))
     km = 6367 * c  # 6367 km is the radius of the Earth
-    return km   
+    return km
 
 '''
 Square box around coordinates given distance
@@ -66,7 +65,7 @@ def boundingBox(d,lat,lon):
      lat1sup = lat + (d*3.531426/500)
      lon1sup = lon + (d*3.531426/500)
      lat1inf = lat - (d*3.531426/500)
-     lon1inf = lon - (d*3.531426/500)     
+     lon1inf = lon - (d*3.531426/500)
      return lat1inf,lat1sup,lon1inf,lon1sup
 
 '''
@@ -89,14 +88,14 @@ def alertPhones(dat_inf,dat_sup,lat,lon,radius,request_size=100000,table="bigtab
     print "size ok"
     box = boundingBox(radius,lon,lat)
     print "box ok"
-    res_coor = session.execute("select latitude,longitude,timestamp,phone from "+table+" where token(timestamp,latitude,longitude)>=token('"+str(dat_inf)+"',"+str(box[0])+","+str(box[2])+") and token(timestamp,latitude,longitude)<token('"+str(dat_sup)+"',"+str(box[1])+","+str(box[3])+");")   
+    res_coor = session.execute("select latitude,longitude,timestamp,phone from "+table+" where token(timestamp,latitude,longitude)>=token('"+str(dat_inf)+"',"+str(box[0])+","+str(box[2])+") and token(timestamp,latitude,longitude)<token('"+str(dat_sup)+"',"+str(box[1])+","+str(box[3])+");")
     print "Session ok"
     phonelist = list()
     for i in range(0,len(res_coor)):
 	lat2 = res_coor[i][0]
         lon2 = res_coor[i][1]
-        date = res_coor[i][2]    
-        phone = str(res_coor[i][3]).split('.')[0]     
+        date = res_coor[i][2]
+        phone = str(res_coor[i][3]).split('.')[0]
         if(haversine(lon,lat,lon2,lat2)<=radius and timestampInterval(date,dat_inf,dat_sup)):
             phonelist.append((lat2,lon2,date,phone))
     print phonelist[1]
@@ -106,7 +105,7 @@ def alertPhones(dat_inf,dat_sup,lat,lon,radius,request_size=100000,table="bigtab
 Convert CSV file into dataframe
 '''
 def fromCSVtoDataframe(csvfile=''):
-    data =  pd.read_csv(csvfile,sep=';',names=['timestamp','codegsm','latitude','longitude','phone'])   
+    data =  pd.read_csv(csvfile,sep=';',names=['timestamp','codegsm','latitude','longitude','phone'])
     data['timestamp'] = data['timestamp'].apply(lambda x: x.split(',')[0])
     return data
 
@@ -119,10 +118,10 @@ def multipleInsertCreation(data,table="bigtable"):
         codegsm = data.iloc[i][1]
         latitude = data.iloc[i][2]
         longitude = data.iloc[i][3]
-        phone = data.iloc[i][4]    
+        phone = data.iloc[i][4]
         temp = "insert into "+table+"(timestamp,latitude,longitude,codegsm,phone) VALUES('"+str(timestamp)+"',"+str(latitude)+","+str(longitude)+",'"+str(codegsm)+"','"+str(phone)+"')"
-        cmd = cmd + " " + temp + " \n " 
-        print i
+        cmd = cmd + " " + temp + " \n "
+
     return begin + cmd + end
 
 def multipleInsertCreation2(data,table="bigtable"):
@@ -136,13 +135,13 @@ def multipleInsertCreation2(data,table="bigtable"):
         longitude = data[i][1]
         temp = "insert into "+table+"(timestamp,latitude,longitude,phone) VALUES('"+str(timestamp)+"',"+str(latitude)+","+str(longitude)+",'"+str(phone)+"')"
         cmd = cmd + " " + temp + " \n "
-        print i
+
     return begin + cmd + end
 
 
 def multipleInsertExec(keyspace="",cmd=""):
     cluster = Cluster()
-    session = cluster.connect(keyspace)   
+    session = cluster.connect(keyspace)
     session.execute(cmd)
 
 #####################################################################
@@ -177,4 +176,3 @@ for i in range(0,100):
         multipleInsertExec(keyspace="japtest2",cmd=test)
         print "-------------------------------------------------"
 '''
-
