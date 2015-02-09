2a188490cd672a851e3a971c32be98cec0923acc
franblas <francois_blas@hotmail.com>
2015-02-09 23:54:48 +0100

    Update cassandraTest.py

--- Cassandra/cassandraTest.py
+++ Cassandra/cassandraTest.py
@@ -34,18 +34,6 @@ def insertCassandra(session,timestamp,latitude,longitude,table="",codegsm="",pho
     session.execute("insert into "+table+"(timestamp,latitude,longitude,codegsm,phone) VALUES('"+str(timestamp)+"',"+str(latitude)+","+str(longitude)+",'"+str(codegsm)+"','"+str(phone)+"');")
 
 '''
-Insert all rows from panda dataframe to Cassandra table
-'''
-def fillTable(session,data,table=""):
-    for i in range(0,len(data.index)):
-        timestamp = data.iloc[i][0]
-        codegsm = data.iloc[i][1]
-        latitude = data.iloc[i][2]
-        longitude = data.iloc[i][3]
-        phone = data.iloc[i][4]
-        insertCassandra(session,timestamp,latitude,longitude,table,codegsm,phone)
-
-'''
 Calculate Haversine distance of 2 points
 '''
 def haversine(lon1, lat1, lon2, lat2):
@@ -58,40 +46,14 @@ def haversine(lon1, lat1, lon2, lat2):
     return km
 
 '''
-Square box around coordinates given distance
-d : distance (in km)
-'''
-def boundingBox(d,lat,lon):
-     lat1sup = lat + (d*3.531426/500)
-     lon1sup = lon + (d*3.531426/500)
-     lat1inf = lat - (d*3.531426/500)
-     lon1inf = lon - (d*3.531426/500)
-     return lat1inf,lat1sup,lon1inf,lon1sup
-
-'''
-Tell if a date is in a range of 2 dates
-'''
-def timestampInterval(date,dat_inf,dat_sup):
-    if(dat_inf<date<=dat_sup):
-        return True
-    else:
-        return False
-
-'''
 Get people datas to prevent from the tsunami
 '''
 def alertPhones(dat_inf,dat_sup,lat,lon,radius,request_size=100000,table="bigtable4",keyspace="japantsunami"):
     cluster = Cluster()
     session = cluster.connect(keyspace)
-    print "Connexion Ok"
-    session.default_timeout = 60
+    session.default_timeout = 30
     session.default_fetch_size = request_size
-    print "size ok"
-    #box = boundingBox(radius,lon,lat)
-    print "box ok"
     res_coor = session.execute("select latitude,longitude,timestamp,phone from "+table+" where timestamp>='"+str(dat_inf)+"' and timestamp<'"+str(dat_sup)+"' allow filtering;")
-    #"+str(box[0])+","+str(box[2])+""+str(box[1])+","+str(box[3])+")
-    print "Session ok"
     phonelist = list()
     for i in range(0,len(res_coor)):
 	lat2 = res_coor[i][0]
@@ -100,33 +62,9 @@ def alertPhones(dat_inf,dat_sup,lat,lon,radius,request_size=100000,table="bigtab
         phone = str(res_coor[i][3]).split('.')[0]
         if(haversine(lon,lat,lon2,lat2)<=radius):
             phonelist.append((lat2,lon2,date,phone))
-    print phonelist[1]
     return phonelist
 
-'''
-Convert CSV file into dataframe
-'''
-def fromCSVtoDataframe(csvfile=''):
-    data =  pd.read_csv(csvfile,sep=';',names=['timestamp','codegsm','latitude','longitude','phone'])
-    data['timestamp'] = data['timestamp'].apply(lambda x: x.split(',')[0])
-    return data
-
 def multipleInsertCreation(data,table="bigtable"):
-    begin = "BEGIN BATCH \n "
-    end = " APPLY BATCH;"
-    cmd = ""
-    for i in range(0,len(data.index)-1):
-        timestamp = data.iloc[i][0]
-        codegsm = data.iloc[i][1]
-        latitude = data.iloc[i][2]
-        longitude = data.iloc[i][3]
-        phone = data.iloc[i][4]
-        temp = "insert into "+table+"(timestamp,latitude,longitude,codegsm,phone) VALUES('"+str(timestamp)+"',"+str(latitude)+","+str(longitude)+",'"+str(codegsm)+"','"+str(phone)+"')"
-        cmd = cmd + " " + temp + " \n "
-
-    return begin + cmd + end
-
-def multipleInsertCreation2(data,table="bigtable"):
     begin = "BEGIN BATCH \n "
     end = " APPLY BATCH;"
     cmd = ""
@@ -137,44 +75,10 @@ def multipleInsertCreation2(data,table="bigtable"):
         longitude = data[i][1]
         temp = "insert into "+table+"(timestamp,latitude,longitude,phone) VALUES('"+str(timestamp)+"',"+str(latitude)+","+str(longitude)+",'"+str(phone)+"')"
         cmd = cmd + " " + temp + " \n "
-
     return begin + cmd + end
-
 
 def multipleInsertExec(keyspace="",cmd=""):
     cluster = Cluster()
     session = cluster.connect(keyspace)
     session.execute(cmd)
 
-#####################################################################
-
-yop = list((12,11,'yop','po'))
-yop.append((13,1313,'fsf','gdg'))
-yop.append((4648,456,'gfdg','ff'))
-
-print yop[0:1]
-
-'''
-cluster = Cluster()
-session = cluster.connect()
-createKeyspaceCassandra(session,keyspace="japtest2")
-
-session = cluster.connect('japtest2')
-createTableCassandra(session,table='bigtable')
-
-datacsv = fromCSVtoDataframe('data1MB.csv')
-#insertion = multipleInsertCreation(datas,table='bigtable')
-#multipleInsertExec(keyspace='japtest2',cmd=insertion)
-offset = 1000
-for i in range(0,100):
-    if(i == 0):
-        test = multipleInsertCreation(data=datacsv[0:offset],table="bigtable")
-        multipleInsertExec(keyspace="japtest2",cmd=test)
-        print "-------------------------------------------------"
-    else:
-        u = i*offset
-        uu = u + offset
-        test = multipleInsertCreation(data=datacsv[u:uu],table="bigtable")
-        multipleInsertExec(keyspace="japtest2",cmd=test)
-        print "-------------------------------------------------"
-'''
