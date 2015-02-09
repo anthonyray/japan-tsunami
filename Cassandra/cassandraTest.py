09a466b8ea5b6a5043637dd6e1a88e78b94eedbc
franblas <francois_blas@hotmail.com>
2015-02-10 00:47:18 +0100

    Update cassandraTest.py

--- Cassandra/cassandraTest.py
+++ Cassandra/cassandraTest.py
@@ -57,10 +57,10 @@ def alertPhones(dat_inf,dat_sup,lat,lon,radius,request_size=100000,table="bigtab
     phonelist = list()
     for i in range(0,len(res_coor)):
 	lat2 = res_coor[i][0]
-	print lat2
         lon2 = res_coor[i][1]
         date = res_coor[i]
         phone = str(res_coor[i][3]).split('.')[0]
+        print haversine(lon,lat,lon2,lat2)
         if(haversine(lon,lat,lon2,lat2)<=radius):
             print 'OKKK'	
             phonelist.append((lat2,lon2,date,phone))
