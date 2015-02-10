4c3be8b7631e663cdd5de5560704d509492e59b2
anthonyray <dantho361@hotmail.com>
2015-02-10 02:19:50 +0100

    Parsing dates

---
+++ final/cassandraTest.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+"""
+reated on Tue Jan 20 16:17:43 2015
+@author: Paco
+"""
+"""
+------------------------------------------------
+--- Japan Tsunami Project
+--- Cassandra drivers and phone alert
+------------------------------------------------
+"""
+
+#import pandas as pd
+from datetime import datetime
+from math import radians, cos, sin, asin, sqrt
+from cassandra.cluster import Cluster
+
+def createKeyspaceCassandra(session,keyspace=""):
+    session.execute("CREATE KEYSPACE "+keyspace+" WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor' : 5};")
+    print "Keyspace "+keyspace+" created !";
+
+'''
+Create a special table into Cassandra database
+'''
+def createTableCassandra(session,table=""):
+    session.execute("CREATE TABLE "+table+"(timestamp timestamp, codegsm text, latitude float, longitude float, phone text, PRIMARY KEY((codegsm),timestamp));")
+    print "Table "+table+" created !";
+
+'''
+Insert one row into a table
+'''
+def insertCassandra(session,timestamp,latitude,longitude,table="",codegsm="",phone=""):
+    session.execute("insert into "+table+"(timestamp,latitude,longitude,codegsm,phone) VALUES('"+str(timestamp)+"',"+str(latitude)+","+str(longitude)+",'"+str(codegsm)+"','"+str(phone)+"');")
+
+'''
+Calculate Haversine distance of 2 points
+'''
+def haversine(lon1, lat1, lon2, lat2):
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    km = 6367 * c  # 6367 km is the radius of the Earth
+    return km
+
+'''
+Get people datas to prevent from the tsunami
+'''
+def alertPhones(dat_inf,dat_sup,lat,lon,radius,request_size=100000,table="bigtable4",keyspace="japantsunami"):
+    cluster = Cluster()
+    session = cluster.connect(keyspace)
+    session.default_timeout = 30
+    session.default_fetch_size = request_size
+    res_coor = session.execute("select latitude,longitude,timestamp,phone,codegsm from "+table+" where timestamp>='"+str(dat_inf)+"' and timestamp<'"+str(dat_sup)+"' allow filtering;")
+    phonelist = list()
+    for i in range(0,len(res_coor)):
+	lon2 = res_coor[i][0]
+        lat2 = res_coor[i][1]
+        date = res_coor[i][2]
+        phone = str(res_coor[i][3])
+        codegsm = str(res_coor[i][4])
+        if(haversine(lon,lat,lon2,lat2)<=radius):
+            print lat2,lon2,date,phone,codegsm
+            phonelist.append((lat2,lon2,date,phone,codegsm))
+    return phonelist
+
+def multipleInsertCreation(data,table="bigtable"):
+    begin = "BEGIN BATCH \n "
+    end = " APPLY BATCH;"
+    cmd = ""
+    for i in range(0,len(data)-1):
+        timestamp = data[i][2]
+        phone = data[i][3]
+        latitude = data[i][0]
+        longitude = data[i][1]
+        codegsm = data[i][4]
+        temp = "insert into "+table+"(timestamp,latitude,longitude,phone,codegsm) VALUES('"+str(timestamp)+"',"+str(latitude)+","+str(longitude)+",'"+str(phone)+"','"+str(codegsm)+"')"
+        cmd = cmd + " " + temp + " \n "
+    return begin + cmd + end
+
+def multipleInsertExec(keyspace="",cmd=""):
+    cluster = Cluster()
+    session = cluster.connect(keyspace)
+    session.execute(cmd)
--- final/server.py
+++ final/server.py
@@ -11,19 +11,15 @@ import os
 import cities
 from datetime import datetime
 from flask import Flask, render_template, request,jsonify
+import cassandraTest as cassandre
+import time
+import datetime
+
 
 tmpl_dir = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__, template_folder=tmpl_dir)
 
 japan_cities = getattr(cities,"cities")
-
-"""datinf = datetime(2015, 1, 5, 10, 32)
-datsup = datetime(2015, 1, 25, 10, 32)
-lat_tsunami = 34.241238
-lon_tsunami = 137.433197
-rad = 500
-datas = ca.alertPhones(datinf,datsup,lat_tsunami,lon_tsunami,rad)
-"""
 
 @app.route('/')
 def index():
@@ -34,11 +30,19 @@ def index():
 
 @app.route('/tsunami',methods=["POST"])
 def tsunami():
+    # Parsing the request
     lon = request.form["lon"]
     lat = request.form["lat"]
-    dat = request.form["date"]
+    dat1 = datetime.datetime.strptime(request.form["date"],"%Y-%m-%dT%H:%M")
+    dat2 = dat1 + datetime.timedelta(minutes=10)
+    radius = 500
+    size_req_t = 1000000
 
+    keyspace = "jap1"
+    table = "bigtable"
+    
+    phones = cassandre.alertPhones(dat1,dat2,lat,lon,radius,size_req_t,table,keyspace)
 
-    return jsonify(status="node_down",node="todo")
+    return jsonify(status="node_down",phones=phones)
 
 app.run()
